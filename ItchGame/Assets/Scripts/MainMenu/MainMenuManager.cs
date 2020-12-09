d61e6399672222d7f9ee3fd302ec65e2d9ca1a7b
void <k.koelewijn2@gmail.com>
2020-12-09 11:15:42 +0100

    linked controls scene pt1

--- ItchGame/Assets/Scripts/MainMenu/MainMenuManager.cs
+++ ItchGame/Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -8,28 +8,37 @@ using UnityEngine.SceneManagement;
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private Button StartB;
+    [SerializeField] private Button ControlsB;
     [SerializeField] private Button OptionsB;
     [SerializeField] private Button QuitB;
 
-    void Start()
+    [SerializeField, Range(1, 10)] private int m_startMultiplayerSceneIndex = 0;
+    [SerializeField, Range(1, 10)] private int m_controlsSceneIndex = 0;
+    [SerializeField, Range(1, 10)] private int m_optionsSceneIndex = 0;
+
+    private void Start()
     {
         StartB.onClick.AddListener(GoToMultiplayer);
+        ControlsB.onClick.AddListener(GoToControls);
         OptionsB.onClick.AddListener(GoToOptions);
         QuitB.onClick.AddListener(QuitTheGame);
     }
 
-    void GoToMultiplayer()
-    {
-        SceneManager.LoadScene(1);
-    }
+    private void GoToMultiplayer() => SceneManager.LoadScene(m_startMultiplayerSceneIndex);
 
-    void GoToOptions()
-    {
+    private void GoToControls() => SceneManager.LoadScene(m_controlsSceneIndex);
 
-    }
+    private void GoToOptions() => SceneManager.LoadScene(m_optionsSceneIndex);
 
-    void QuitTheGame()
-    {
-        Application.Quit();
-    }
+#if !UNITY_EDITOR
+    // quits the game in build
+    private void QuitTheGame() => Application.Quit();
+#endif
+
+#if UNITY_EDITOR
+
+    // stops the editor playing the game
+    private void QuitTheGame() => UnityEditor.EditorApplication.isPlaying = false;
+
+#endif
 }
