74a988c010f8ecf215698e7ee3cd529cb1d118e7
void <k.koelewijn2@gmail.com>
2021-01-26 16:06:38 +0100

    pickup fix WIP

---
+++ ItchGame/Assets/Scripts/PlayerSpawnBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerSpawnBehaviour : MonoBehaviourPunCallbacks
+{
+    private Dictionary<int, GameObject> m_PlayerDict = new Dictionary<int, GameObject>();
+
+    private PhotonView m_photonView = null;
+
+    internal IReadOnlyDictionary<int, GameObject> GetPlayerDict => m_PlayerDict;
+
+    private void Start()
+    {
+        // kill rigidbody on players that are not the local
+        if (!photonView.IsMine)
+        {
+            Destroy(GetComponent<Rigidbody2D>());
+        }
+
+        m_photonView = GetComponent<PhotonView>();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        base.OnPlayerEnteredRoom(newPlayer);
+        m_PlayerDict.Add(m_photonView.ViewID, gameObject);
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        base.OnPlayerLeftRoom(otherPlayer);
+        m_PlayerDict.Remove(m_photonView.ViewID);
+    }
+}
